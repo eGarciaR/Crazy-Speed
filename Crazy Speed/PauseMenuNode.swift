7c6a71a7b79f41d3dc817ee84dd6c0463df16b17
Eric Garcia Ribera <e.garcia.ribera@gmail.com>
2016-03-10 00:22:11 +0100

    Fixed pause menu look&feel

--- Crazy Speed/PauseMenuNode.swift
+++ Crazy Speed/PauseMenuNode.swift
@@ -19,40 +19,19 @@ class PauseMenuNode: SKNode {
         title.zPosition = 7
         pauseNode.addChild(title)
         
-        /*let btnShield = SKSpriteNode(imageNamed: "shield")
-        btnShield.name = "shield"
-        btnShield.position = CGPointMake(pauseNode.size.width/3, -(pauseNode.size.height*0.2))
-        btnShield.setScale(0.75)
-        btnShield.zPosition = 7
-        pauseNode.addChild(btnShield)*/
-        
         let btnResume = SKSpriteNode(imageNamed: "play")
         btnResume.name = "resume"
-        btnResume.position = CGPointMake(0, -(pauseNode.size.height*0.2))
+        btnResume.position = CGPointMake(-(pauseNode.size.width*0.25), -(pauseNode.size.height*0.2))
         btnResume.setScale(0.75)
         btnResume.zPosition = 7
         pauseNode.addChild(btnResume)
         
-        /*let btnSettings = SKSpriteNode(imageNamed: "settings")
-        btnSettings.name = "settings"
-        btnSettings.position = CGPointMake((-pauseNode.size.width/3), -(pauseNode.size.height*0.2))
-        btnSettings.setScale(0.75)
-        btnSettings.zPosition = 7
-        pauseNode.addChild(btnSettings)*/
-        
         let btnReturnMenu = SKSpriteNode(imageNamed: "quit")
         btnReturnMenu.name = "quit"
-        btnReturnMenu.position = CGPointMake(pauseNode.size.width/3, -(pauseNode.size.height*0.2))
+        btnReturnMenu.position = CGPointMake(pauseNode.size.width*0.25, -(pauseNode.size.height*0.2))
         btnReturnMenu.setScale(0.75)
         btnReturnMenu.zPosition = 7
         pauseNode.addChild(btnReturnMenu)
-        
-        /*let btnShots = SKSpriteNode(imageNamed: "shots")
-        btnShots.name = "shots"
-        btnShots.position = CGPointMake((-pauseNode.size.width/3), -(pauseNode.size.height*0.2))
-        btnShots.setScale(0.75)
-        btnShots.zPosition = 7
-        pauseNode.addChild(btnShots)*/
         
         hidden = true
     }
